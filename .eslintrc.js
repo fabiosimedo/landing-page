b61d0153ba17e4c64e2dd650b2c5c83bf83e27b6
Fabio Simedo <fabiosimedo@hotmail.com>
2021-10-12 22:16:42 -0300

    First Commit!

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+//eslint-disable-next-line
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off',
+    'prettier/prettier': ['error', { singleQuote: true, parser: 'flow' }],
+  },
+};
